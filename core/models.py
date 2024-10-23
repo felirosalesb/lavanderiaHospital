78211406bae6d654dcb3934a2183cdbc0f2997ba
felirosalesb <feli.rosales@duocuc.cl>
2024-10-23 12:27:23 -0300

    actualizacin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class ropaLavanderiaHospital(models.Model):
+    
+    limpio = models.IntegerField()
+    suciio = models.IntegerField()
+    cantidad = models.IntegerField()
+
+class ropaLavanderiaExterna(models.Model):
+    
+    limpio = models.IntegerField()
+    suciio = models.IntegerField()
+    cantidad = models.IntegerField()
